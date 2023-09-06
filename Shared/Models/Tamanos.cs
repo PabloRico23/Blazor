ccb562963cd04bbe892a2262d920af6180067082
pablo <pablo@GWNR71517>
2023-09-05 18:23:56 -0600

    Add project files.

---
+++ Shared/Models/Tamanos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBlazor.Shared.Models
+{
+    public class Tamanos
+    {
+        public int id { get; set; }
+        public int Tamano { get; set; }
+        public float Precio { get; set; }
+    }
+}
---
+++ Shared/Models/TipoMasa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBlazor.Shared.Models
+{
+    public class TipoMasa
+    {
+        public int id { get; set; }
+        public int Tipo { get; set; }
+
+        public float Precio { get; set; }
+
+    }
+}
